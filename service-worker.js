09fb72ad72dc472fca1b9df48543691529e1543d
litaibai0601 <cptsytptpd@gmail.com>
2025-07-10 09:19:09 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,12 @@
 const CACHE_NAME = 'mycards-cache-v1';
+
 // 缓存所有核心文件，确保离线可用
 const urlsToCache = [
-  './', // 缓存根路径，即 index.html
+  './',
   './index.html',
   './manifest.json',
-  './service-worker.js'
-  // 如果你将来添加了 icon-192.png 和 icon-512.png，也请在这里添加它们的路径
-  // './icon-192.png',
-  // './icon-512.png'
+  './service-worker.js',
+  './icons/icon-512.png'
 ];
 
 // 安装 Service Worker 并缓存所有资源
@@ -26,12 +25,7 @@ self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
       .then(response => {
-        // 如果缓存中有，则返回缓存的资源
-        if (response) {
-          return response;
-        }
-        // 否则，从网络获取资源
-        return fetch(event.request);
+        return response || fetch(event.request);
       })
   );
 });
@@ -43,7 +37,7 @@ self.addEventListener('activate', event => {
     caches.keys().then(cacheNames => {
       return Promise.all(
         cacheNames.map(cacheName => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
+          if (!cacheWhitelist.includes(cacheName)) {
             console.log('Deleting old cache:', cacheName);
             return caches.delete(cacheName);
           }
